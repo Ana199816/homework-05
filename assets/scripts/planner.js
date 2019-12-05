8e292f194e4efb5863fd8a7ad55fc1fa2a250c36
Ana Suvac <anasuvac@yahoo.com>
2019-12-05 18:11:04 -0500

    fix error

--- assets/scripts/planner.js
+++ assets/scripts/planner.js
@@ -1,29 +1,27 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-// hour variables
+
 
 var hour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
-// add AM/PM to times
 
-// for each function to loop and set elements/classes/attributes.
 hour.forEach(function(time){
     var text = $("<textarea>").attr("dataStorage", time);
     var btn = $("<button>").addClass("saveBtn fas fa-save");
     var row = $("<div>").addClass("row");
     var hours = $("<div>").addClass("hour");
-    // parses hour array 
+  
     let any = parseInt(time);
     
-    // append each variable
+  
 
       row.append(hours, text, btn);
 
-    // appending the row to the provided container class
+ 
 
     $(".container").append(row);
 
-    // checking if time is greater or less than 12 and setting AM/PM
+    
 
     if(any < 12){
         hours.text(time + "am");
@@ -39,18 +37,17 @@ hour.forEach(function(time){
 
 });
 
-    // will not perform any functions until entire page is loaded.
+   
 
 $(document).ready( () => {
     
-    // button to save to local storage on click - .prev selects the previous index of the button which is the textarea in this case.
+    
 
     $(".saveBtn").on("click", function(){
         localStorage.setItem($(this).prev("textarea").attr("dataStorage"),$(this).prev("textarea").val());
     });
 
-    // "*" means grab all within the data attr and return from local storage.
-
+    
     $("*[dataStorage]").each(function(){
         $(this).val(localStorage.getItem($(this).attr("dataStorage"))
     )});
